2f0d87957b5b5fcfa4dfc12a553d4a2b10c934af
Alberto Dragoni <albertodragoni@yahoo.it>
2020-02-17 16:00:38 +0100

    mission 1 ok

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var nome = prompt('Nome')
+var cognome = prompt ('Cognome')
+var colorePreferito = prompt ('Colore preferito')
+document.getElementById('psw').innerHTML = nome + cognome + colorePreferito + 20
