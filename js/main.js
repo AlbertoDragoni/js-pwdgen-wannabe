1c7115c0d24873fc70ada76611d6b78bdf919088
Alberto Dragoni <albertodragoni@yahoo.it>
2020-02-17 17:15:55 +0100

    con virgolette

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ var cognome = prompt ('Cognome');
 var colorePreferito = prompt ('Colore preferito');
 var data = new Date ()
 var annoCorrente = data.getFullYear();
-document.getElementById('psw').innerHTML = nome + cognome + colorePreferito + annoCorrente;
+document.getElementById('psw').innerHTML = nome + ';' + cognome + ';' + colorePreferito + ';' + annoCorrente;
 document.getElementById('container').setAttribute('class','visible');
