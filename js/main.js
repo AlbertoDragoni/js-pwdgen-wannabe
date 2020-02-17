239b835f8c39932e1d0dcfcddbad8499d123db5c
Alberto Dragoni <albertodragoni@yahoo.it>
2020-02-17 16:11:19 +0100

    con sparimento

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var nome = prompt('Nome')
-var cognome = prompt ('Cognome')
-var colorePreferito = prompt ('Colore preferito')
-document.getElementById('psw').innerHTML = nome + cognome + colorePreferito + 20
+var nome = prompt('Nome');
+var cognome = prompt ('Cognome');
+var colorePreferito = prompt ('Colore preferito');
+document.getElementById('psw').innerHTML = nome + cognome + colorePreferito + 20;
+document.getElementById('container').setAttribute('class','visible');
